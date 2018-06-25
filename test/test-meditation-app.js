2b429c6b803b947c51f15074724411ae8030c820
Jayshol <jayshol@gmail.com>
2018-06-25 03:04:35 -0700

    Added tests

---
+++ test/test-meditation-app.js
@@ -0,0 +1,30 @@
+'use strict';
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const {app, runServer, closeServer} = require('../server');
+
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+describe('Meditation-app API resource', function(){
+	before(function(){
+		return runServer();
+	});
+
+	after(function(){
+		return closeServer();
+	});
+
+	describe('Root test', function(){
+		it('should return index.html', function(){
+			return chai.request(app)
+			.get('/')
+			.then(function(res){
+				expect(res).to.have.status(200);
+				expect(res).to.be.html;
+			});
+		});
+	}); //root test
+}); // Main describe
